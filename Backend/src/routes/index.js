2043f67a2ab0ec4a2623bcbec0532f0df9a45c0f
Akshay Gupta <akshaygupta1297@gmail.com>
2026-04-20 00:05:30 +0530

    Added logger in health route

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const logger = require("../config/logger");
 const app = express();
 
 app.get("/health", (req, res) => {
-  res.json({
+  const healthCheckResponse = {
     status: "ok",
     uptime: process.uptime(), // how long server has been running
     timestamp: Date.now(), // current time
     memoryUsage: process.memoryUsage(), // optional: memory details
-  });
+  };
+  logger.info(`helth check api response ${JSON.stringify(healthCheckResponse)}`);
+  res.json(healthCheckResponse);
 });
 
 app.use((req, res, next) => {
